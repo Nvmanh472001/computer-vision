dfb1b3a1a63b6eb929c490fc1a5ec05e341e074a
VietTran2707 <tranducviet_t64@hus.edu.vn>
2022-01-15 18:49:40 +0700

    last client

---
+++ client/HandelImage.py
@@ -0,0 +1,21 @@
+import os
+import streamlit
+
+def callAPI(model, imageDocker, imagePath):
+    cmd = f'cd ../server && sudo cog predict {imageDocker}:latest -i image=@{imagePath} -i model={model} && mv -f ./output.png ../client/image/{model}'    
+    os.system(cmd)
+    
+def HandlelImage(image_file, model):
+    img_save_path = os.path.join("../server/content/real/{}".format(model), image_file.name)
+    
+    with open(img_save_path, "wb") as f:
+                f.write((image_file).getbuffer())    
+            
+    img_late = "./image/{}/output.png".format(model)
+    img_path_server = "./content/real/{}/".format(model) + image_file.name
+    callAPI(model, 'tvm-com-vision', img_path_server)
+    
+    cmd_remove = f'rm {img_save_path}'
+    os.system(cmd_remove)
+    
+    return img_late
--- client/app.py
+++ client/app.py
@@ -1,16 +1,12 @@
 import os
 import streamlit as st
 from PIL import Image
-
-def callAPI(model, imageDocker, imagePath):
-    cmd = f'cd ../server && sudo cog predict {imageDocker}:latest -i image=@{imagePath} -i model={model} && mv -f ./output.png ../client/image/{model}'    
-    os.system(cmd)
+from HandelImage import HandlelImage
 
 def load_image(image_file):
     img = Image.open(image_file)
     return img
-    
-    
+
 def main():
     st.set_page_config(layout="wide")
     
@@ -21,7 +17,7 @@ def main():
     st.sidebar.header('Selection')
     choice = st.empty()
     choice = st.sidebar.selectbox('', menu)
-
+        
     # Create the Home page
     if choice == 'CONVERT TO DIGITAL IMAGE':
         image_file = st.file_uploader('CONVERT TO DIGITAL IMAGE',type=['jpeg', 'jpg', 'jpe','png', 'bmp'], key=1)
@@ -30,44 +26,48 @@ def main():
             col1, col2 = st.columns(2)
             
             img = load_image(image_file) 
-            img_save_path = os.path.join("../server/content/real/gan", image_file.name)
-            
+            img_late = ''
+               
             with col1:
                 st.image(img)
+                if st.button("COVERT"):
+                    img_late = HandlelImage(image_file, "gan")
             
-            with open(img_save_path, "wb") as f:
-                f.write((image_file).getbuffer())    
-            
-            img_late = "./image/gan/output.png"
-            img_path = "./content/real/gan/" + image_file.name
-            callAPI("gan", 'tvm-com-vision', img_path)
-              
             with col2:
                 if os.path.exists(img_late) and os.path.isfile(img_late):
                     st.image(img_late)
+                    with open(img_late, "rb") as image:
+                        btn = st.download_button(
+                                label="Download image",
+                                data= image,
+                                file_name="output.png",
+                                mime="image/png"
+                            )
 
     elif choice == 'OBJECT DECTECTION':
         image_file = st.file_uploader('OBJECT DECTECTION',type=['jpeg', 'jpg', 'jpe','png', 'bmp'], key=2)
+        
         if image_file is not None:
             col1, col2 = st.columns(2)
             
             img = load_image(image_file) 
-            img_save_path = os.path.join("../server/content/real/detection", image_file.name)
+            img_late = ''
             
             with col1:
                 st.image(img)
+                if st.button("DETECT"):
+                    img_late = HandlelImage(image_file, "detection")
             
-            with open(img_save_path, "wb") as f:
-                f.write((image_file).getbuffer())    
-            
-            img_late = "./image/detection/output.png"
-            img_path = "./content/real/detection/" + image_file.name
-            callAPI("detection", 'tvm-com-vision', img_path)
-              
             with col2:
                 if os.path.exists(img_late) and os.path.isfile(img_late):
                     st.image(img_late)
-                
+                    with open(img_late, "rb") as image:
+                        btn = st.download_button(
+                                label="Download image",
+                                data= image,
+                                file_name="output.png",
+                                mime="image/png"
+                            )
 if __name__ == '__main__':
     main()
     
